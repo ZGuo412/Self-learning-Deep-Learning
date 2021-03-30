95b54bc5dc44784ed7de54e50d7fffe946d4f2d6
Ziyu Guo <guo412@purdue.edu>
2021-03-31 07:10:26 +0900

    Finished the Chapter 2 exercise 1

---
+++ dp-pytorch/Chapter2_exercise.py
@@ -0,0 +1,119 @@
+##1. Feed the image of the golden retriever into the horse-to-zebra model
+
+import torch
+import torch.nn as nn
+from torchvision import transforms
+from PIL import Image
+
+
+##Prepare the Resnet generator model
+class ResNetBlock(nn.Module): # <1>
+
+    def __init__(self, dim):
+        super(ResNetBlock, self).__init__()
+        self.conv_block = self.build_conv_block(dim)
+
+    def build_conv_block(self, dim):
+        conv_block = []
+
+        conv_block += [nn.ReflectionPad2d(1)]
+
+        conv_block += [nn.Conv2d(dim, dim, kernel_size=3, padding=0, bias=True),
+                       nn.InstanceNorm2d(dim),
+                       nn.ReLU(True)]
+
+        conv_block += [nn.ReflectionPad2d(1)]
+
+        conv_block += [nn.Conv2d(dim, dim, kernel_size=3, padding=0, bias=True),
+                       nn.InstanceNorm2d(dim)]
+
+        return nn.Sequential(*conv_block)
+
+    def forward(self, x):
+        out = x + self.conv_block(x) # <2>
+        return out
+
+
+class ResNetGenerator(nn.Module):
+
+    def __init__(self, input_nc=3, output_nc=3, ngf=64, n_blocks=9): # <3>
+
+        assert(n_blocks >= 0)
+        super(ResNetGenerator, self).__init__()
+
+        self.input_nc = input_nc
+        self.output_nc = output_nc
+        self.ngf = ngf
+
+        model = [nn.ReflectionPad2d(3),
+                 nn.Conv2d(input_nc, ngf, kernel_size=7, padding=0, bias=True),
+                 nn.InstanceNorm2d(ngf),
+                 nn.ReLU(True)]
+
+        n_downsampling = 2
+        for i in range(n_downsampling):
+            mult = 2**i
+            model += [nn.Conv2d(ngf * mult, ngf * mult * 2, kernel_size=3,
+                                stride=2, padding=1, bias=True),
+                      nn.InstanceNorm2d(ngf * mult * 2),
+                      nn.ReLU(True)]
+
+        mult = 2**n_downsampling
+        for i in range(n_blocks):
+            model += [ResNetBlock(ngf * mult)]
+
+        for i in range(n_downsampling):
+            mult = 2**(n_downsampling - i)
+            model += [nn.ConvTranspose2d(ngf * mult, int(ngf * mult / 2),
+                                         kernel_size=3, stride=2,
+                                         padding=1, output_padding=1,
+                                         bias=True),
+                      nn.InstanceNorm2d(int(ngf * mult / 2)),
+                      nn.ReLU(True)]
+
+        model += [nn.ReflectionPad2d(3)]
+        model += [nn.Conv2d(ngf, output_nc, kernel_size=7, padding=0)]
+        model += [nn.Tanh()]
+
+        self.model = nn.Sequential(*model)
+
+    def forward(self, input): # <3>
+        return self.model(input)
+
+
+##Create the netG model
+netG = ResNetGenerator()
+
+
+##open the input pickle file and load the input to the model I just created by using load_state_dict method
+
+model_path = "horse2zebra_0.4.0.pth"
+model_data = torch.load(model_path)
+netG.load_state_dict(model_data)
+
+netG.eval()
+
+##Define the preprocessing function
+
+preprocess = transforms.Compose([transforms.Resize(256),
+                                 transforms.ToTensor()])
+
+##Load the gold retriever image and put it into the net model
+
+img = Image.open("bobby.jpg")
+
+##Resize the image and transfer it to tenser
+
+img_t = preprocess(img)
+batch_t = torch.unsqueeze(img_t, 0)
+
+out = netG(batch_t)
+
+#convert the output data to the image
+
+out_t = (out.data.squeeze() + 1.0) / 2
+out_img = transforms.ToPILImage()(out_t)
+out_img.show()
+
+
+##2. Search hubconf.py file on Github
