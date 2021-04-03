217a7bedee5c2b761f4c1bb7fe6491d3a259aa26
ZGuo412 <guozy@g.ecc.u-tokyo.ac.jp>
2021-04-03 16:52:29 +0900

    self-learning named_tensor initializing for an image and batch of images

--- dp-pytorch/Section_3.4_named_tensor.py
+++ dp-pytorch/Section_3.4_named_tensor.py
@@ -7,3 +7,15 @@ weights = torch.tensor([0.2126, 0.7152, 0.0722]) #list to tensor array
 
 
 batch_t = torch.randn(2,3,5,5) #refers to batch, channel, rows and columns
+
+#get the unweighted mean from the image, batch of images data
+#averafe of channel
+img_gray_naive = img_t.mean(-3)
+batch_gray_naive = batch_t.mean(-3)
+
+print(img_t, img_gray_naive)
+
+
+
+
+
